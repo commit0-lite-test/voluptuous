d68527a5cfc59da1587bb9214f018afbb7e8e0cb
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 13:36:13 -0800

    fix: Update imports and formatting in voluptuous/util.py

--- voluptuous/util.py
+++ voluptuous/util.py
@@ -1,9 +1,9 @@
 import typing
-from voluptuous import validators
-from voluptuous.error import Invalid, LiteralInvalid, TypeInvalid
-from voluptuous.schema_builder import DefaultFactory
-from voluptuous.schema_builder import Schema, default_factory, raises
-__author__ = 'tusharmakkar08'
+from voluptuous.error import LiteralInvalid, TypeInvalid
+from voluptuous.schema_builder import default_factory
+
+__author__ = "tusharmakkar08"
+
 
 def Lower(v: str) -> str:
     """Transform a string to lower case.
@@ -14,6 +14,7 @@ def Lower(v: str) -> str:
     """
     return v.lower()
 
+
 def Upper(v: str) -> str:
     """Transform a string to upper case.
 
@@ -22,6 +23,7 @@ def Upper(v: str) -> str:
     'HI'
     """
     return v.upper()
+
 
 def Capitalize(v: str) -> str:
     """Capitalise a string.
@@ -32,6 +34,7 @@ def Capitalize(v: str) -> str:
     """
     return v.capitalize()
 
+
 def Title(v: str) -> str:
     """Title case a string.
 
@@ -41,6 +44,7 @@ def Title(v: str) -> str:
     """
     return v.title()
 
+
 def Strip(v: str) -> str:
     """Strip whitespace from a string.
 
@@ -49,6 +53,7 @@ def Strip(v: str) -> str:
     'hello world'
     """
     return v.strip()
+
 
 class DefaultTo(object):
     """Sets a value to default_value if none provided.
@@ -61,7 +66,7 @@ class DefaultTo(object):
     []
     """
 
-    def __init__(self, default_value, msg: typing.Optional[str]=None) -> None:
+    def __init__(self, default_value, msg: typing.Optional[str] = None) -> None:
         self.default_value = default_factory(default_value)
         self.msg = msg
 
@@ -71,7 +76,8 @@ class DefaultTo(object):
         return v
 
     def __repr__(self):
-        return 'DefaultTo(%s)' % (self.default_value(),)
+        return "DefaultTo(%s)" % (self.default_value(),)
+
 
 class SetTo(object):
     """Set a value, ignoring any previous value.
@@ -90,7 +96,8 @@ class SetTo(object):
         return self.value()
 
     def __repr__(self):
-        return 'SetTo(%s)' % (self.value(),)
+        return "SetTo(%s)" % (self.value(),)
+
 
 class Set(object):
     """Convert a list into a set.
@@ -104,27 +111,27 @@ class Set(object):
     ...   s([set([1, 2]), set([3, 4])])
     """
 
-    def __init__(self, msg: typing.Optional[str]=None) -> None:
+    def __init__(self, msg: typing.Optional[str] = None) -> None:
         self.msg = msg
 
     def __call__(self, v):
         try:
             set_v = set(v)
         except Exception as e:
-            raise TypeInvalid(self.msg or 'cannot be presented as set: {0}'.format(e))
+            raise TypeInvalid(self.msg or "cannot be presented as set: {0}".format(e))
         return set_v
 
     def __repr__(self):
-        return 'Set()'
+        return "Set()"
+
 
 class Literal(object):
-
     def __init__(self, lit) -> None:
         self.lit = lit
 
-    def __call__(self, value, msg: typing.Optional[str]=None):
+    def __call__(self, value, msg: typing.Optional[str] = None):
         if self.lit != value:
-            raise LiteralInvalid(msg or '%s not match for %s' % (value, self.lit))
+            raise LiteralInvalid(msg or "%s not match for %s" % (value, self.lit))
         else:
             return self.lit
 
